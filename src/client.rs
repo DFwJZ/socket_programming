7b23d9dbc8208640951aff3e241fd38e8397b387
HaozheZhang0818 <zhang.haozhe1@northeastern.edu>
2023-02-05 16:17:36 -0800

    added authentication feature

--- src/client.rs
+++ src/client.rs
@@ -4,21 +4,70 @@ use std::io;
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
-    let mut message = String::new();
+    let mut credentials = String::new();
 
-    // Read user input from standard input (stdin)
-    println!("Enter your message: ");
-    io::stdin().read_line(&mut message).unwrap();
+    // Read user input of username and password
+    println!("Enter your username: ");
+    io::stdin().read_line(&mut credentials).unwrap();
+    let username = credentials.trim().to_string();
 
-    stream.write(message.as_bytes()).unwrap();
+    credentials = String::new();
+    println!("Enter your password: ");
+    io::stdin().read_line(&mut credentials).unwrap();
+    let password = credentials.trim().to_string();
+
+    // Combine username and password with a space separator
+    let credentials = format!("{} {}", username, password);
+
+    stream.write(credentials.as_bytes()).unwrap();
     // flush any buffered data(buf: message in this case)
     // no data should be left in the message after the flush()
-    stream.flush().unwrap_or_else(|error|{
-        println!("Error sending respons: {:?}", error);
-    });
+    stream.flush().unwrap_or_else(|error| {
+        println!("Error sending responses: {:?}", error);
+    }); 
 
-    let mut response = String::new();
-    stream.read_to_string(&mut response).unwrap();
+    if stream.flush().is_ok() {
+        println!("username and password are sent to server");
+    }
 
-    println!("Response: {}", response);
+    let mut buffer = [0; 512];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+        
+    // split the buffer into two messages based on newline char
+    let message_string = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+    // spliting the message by the newline char will remove the newline char from original sent message from server
+    let message = message_string.split("\r\n").collect::<Vec<_>>();
+
+    let response = message[0];
+    let client_addr = message[1];
+
+    println!("respnse: {}\nclient_address: {}", response, client_addr);
+
+    if response == "Authentication successful!" {
+
+        // read message from the user
+        let mut message = String::new();
+        println!("Enter message to send to server: ");
+        io::stdin().read_line(&mut message).unwrap();
+        message = message.trim().to_string();
+
+        // sned message to server
+        stream.write(message.as_bytes()).unwrap();
+        stream.flush().unwrap_or_else(|error| {
+            println!("Error sending message: {:?}", error);
+        });
+
+        if stream.flush().is_ok() {
+            println!("Okay!")
+        }
+
+        // read confirmation from server
+        let mut response1 = String::new();
+        stream.read_to_string(&mut response1).unwrap();
+
+        println!("Client: Response from server: {}", response1);
+    } else {
+        println!("I am getting {}", response);
+        println!("Authentication failed.");
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,45 +1,113 @@
-use std::net::{TcpListener, TcpStream};
+#[macro_use]
+extern crate lazy_static;
+use std::collections::HashMap;
+use std::sync::Mutex;
+// // use lazy_static::lazy_static;
 use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 use std::thread;
+// use std::io;
 
-/*
-used to handle incoming connections from clients.
-It takes a TcpStream as its argument, hich represents a connection to a client.
-*/
+lazy_static! {
+    // hashmap to store usernames and passwords
+    static ref USERS: Mutex<HashMap<String, String>> = Mutex::new([("user1".to_string(), "password1".to_string()), 
+                                            ("user2".to_string(), "password2".to_string())].iter().cloned().collect());
+
+    // hashmap to store tokens and client addresses
+    static ref TOKENS: Mutex<HashMap<String, String>> = Mutex::new(HashMap::new());
+}
+
+
 fn handle_client(mut stream: TcpStream) {
-    // creates a mutable buffer with a size of 512 bytes, initialized with zeros.
+    let users = match USERS.lock() {
+        Ok(guard) => guard,
+        Err(e) => {
+            println!("Error locking users: {}", e);
+            return;
+        }
+    };
+
+let mut tokens = match TOKENS.lock() {
+        Ok(guard) => guard,
+        Err(e) => {
+            println!("Error locking tokens: {}", e);
+            return;
+        }
+    };
+
+
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    // The .. syntax is the range syntax, which represents an inclusive range of all elements in the array.
-    println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
+    // read the client's message
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    let incoming_message = String::from_utf8_lossy(&buffer[..bytes_read]);
+    let credentials: Vec<&str> = incoming_message.trim().split(" ").collect();
 
-    let message = String::from_utf8_lossy(&buffer[..]);
-    let message = message.trim_end_matches("\r\n\r\n");
-    let uppercase_message = message.to_ascii_uppercase();
+    let username = credentials[0];
+    let password = credentials[1];
 
+    if let Some(stored_password) = users.get(username) {
+        if stored_password == password {
+            // generate token
+            let token = format!("{}:{}", username, password);
 
-    println!("Replied with: {}", uppercase_message);
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", uppercase_message);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+            // store token and client address
+            tokens.insert(token, stream.peer_addr().unwrap().to_string());
+
+            // send success message to client
+            let success = "Authentication successful!\r\n".to_string();
+            stream.write(success.as_bytes()).unwrap();
+            stream.flush().unwrap();
+            
+            if stream.flush().is_ok() {
+                println!("Server: server successually authenticated!")
+            }
+
+            let client_addr = stream.peer_addr().unwrap().to_string();
+            let success_with_addr = format!("{}Client address: {}", success, client_addr);
+            stream.write(success_with_addr.as_bytes()).unwrap();
+            stream.flush().unwrap();
+
+            let mut buffer2 =  [0; 512];
+            let bytes_read2 = stream.read(&mut buffer2).unwrap();
+            let incoming_message2 = String::from_utf8_lossy(&buffer2[..bytes_read2]);
+            println!("Server: Received message from client: {}", incoming_message2);
+  
+            let incoming_message2 = incoming_message2.trim_end_matches("\r\n\r\n");
+            let uppercase_message = incoming_message2.to_ascii_uppercase();
+
+            println!("Server: Response message sent by server: {}", uppercase_message);
+            let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", uppercase_message);
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        } else {
+            // send failure message to client
+            let failure = "Server: Password authentication failed".to_string();
+            stream.write(failure.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+    } else {
+        // send failure message to client
+        let failure = "Server: Username authentication failed".to_string();
+        stream.write(failure.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 
 }
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Listening on port 7878...");
     for stream in listener.incoming() {
-        // 'match' similar to switch in Java/Python
         match stream {
             Ok(stream) => {
-                println!("Accepted connection from {:?}", stream.peer_addr().unwrap());
-                let handle_client_thread = thread::spawn(move || {
+                println!("Accepted connection from {}", stream.peer_addr().unwrap());
+                thread::spawn(|| {
                     handle_client(stream)
                 });
-                handle_client_thread.join().unwrap()
-            }
-            Err(e) => {
-                 println!("Error: {}", e);
-            }
+            },
+            Err(e) => println!("Error: {}", e),
         }
     }
+    
 }
